e3941a8fb937c27ea33022c3a55c594e10672716
nornjuidat <njuidatnor750@gmail.com>
2025-12-02 19:07:11 +0200

    update esp js

--- server/src/models/esp.js
+++ server/src/models/esp.js
@@ -2,8 +2,10 @@ class Esp {
     constructor (db){
         this.db = db ;
     }
-    createAvgsensor(name,avg,potId){
-        let sql = "INSERT INTO SENSORS (sensorname,val_avg,date,pot_id)VALUES(?,?,?,?)"
+   async createAvgsensor(name,avg,potId){
+        let sql = "INSERT INTO SENSORS (sensorname,val_avg,date,pot_id)VALUES(?,?,?,?);"
+         await this.db.execute(sql,[name,avg,new Date().toLocaleDateString("he-IL"),potId])
+
     }
 }
 
